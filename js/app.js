d4078c96fb1012b456392498dc4081a1cb555f63
lucerohospina <lucerojhp@gmail.com>
2018-02-07 23:00:47 -0500

    agrega login con google

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ window.addEventListener('load', function() {
   var popPassword = document.getElementById('password-popover')
   var validateEmail = false;
   var validatePassword = false;
+  var googleLogin = document.getElementById('login-google');
 
   function activeLoginBtn() {
     if (validateEmail && validatePassword) {
@@ -39,11 +40,43 @@ window.addEventListener('load', function() {
       validatePassword = true;
       activeLoginBtn();
       popPassword.classList.add('d-none');
-      // inputPassword.tooltip('hide');
     } else {
       popPassword.classList.remove('d-none');
-      // inputPassword.tooltip('show');
     }
+  });
+
+  // Inicializando Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: 'AIzaSyCoJOxjrwPRCbg65yUOL6u3tHkdjhO8nSY',
+    authDomain: 'insta-collage-b53d7.firebaseapp.com',
+    databaseURL: 'https://insta-collage-b53d7.firebaseio.com',
+    projectId: 'insta-collage-b53d7',
+    storageBucket: 'insta-collage-b53d7.appspot.com',
+    messagingSenderId: '642859271142'
+  };
+  firebase.initializeApp(config);
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().useDeviceLanguage();
+
+  googleLogin.addEventListener('click', function() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   });
 });
 
